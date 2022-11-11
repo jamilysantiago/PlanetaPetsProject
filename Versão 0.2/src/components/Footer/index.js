c984e101218f0ca80db2146d02129a9cddccb6aa
Micael Rodrigues <mrodrigues.20032005@gmail.com>
2022-11-11 20:03:24 -0300

    Atualização

--- Versão 0.2/src/components/Footer/index.js
+++ Versão 0.2/src/components/Footer/index.js
@@ -3,6 +3,7 @@ import './FooterStyles.css'
 import { FaGithub } from 'react-icons/fa'
 import logo from '../../assets/img/Logo2.png'
 import {Link} from 'react-router-dom'
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
     return (
@@ -13,10 +14,10 @@ const Footer = () => {
                 </div>
                 <div className='right'>
                     <h4>Sobre o Projeto</h4>
-                    <p>"Quem não ama cachorros e gatos? Um animalzinho em casa é uma amizade para vida toda! A convivência com pets traz inúmeros benefícios para as pessoas, como: vinculo afetivo, mais alegria em casa e união na família. Mas, o maior benefício que a adoção de animais pode nos trazer é a luta contra o abandono. Então, nós do Projeto Planeta Pets estamos aqui para combater o abandono de animais e ajudar você a encontrar um amigo para a vida toda.
-Adoção é uma escolha de amor."</p>
+                    <p>Adoção é uma escolha de amor.</p>
+                    <SocialIcon className="social" url="https://github.com/idev-company" /> 
                 </div>
-
+                
             </div>
         </div>
     )
