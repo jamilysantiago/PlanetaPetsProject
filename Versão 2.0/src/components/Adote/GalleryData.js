061b7ef577eb4dd213ce3ef9bac66983df357298
Micael Rodrigues <mrodrigues.20032005@gmail.com>
2022-11-24 23:53:12 -0300

    Atualização

--- Versão 2.0/src/components/Adote/GalleryData.js
+++ Versão 2.0/src/components/Adote/GalleryData.js
@@ -13,38 +13,38 @@ export const GalleryData = [
     },
     {
         id:3,
-        titile:"Cachorros",
-        image:"./images/D1.jpg",
+        titile:"Gatos",
+        image:"./images/C3.jpg",
         name: "Rita"
     },
     {
         id:4,
         titile:"Cachorros",
-        image:"./images/D2.jpg",
+        image:"./images/D1.jpg",
         name: "José"
     },
     {
         id:5,
-        titile:"outros",
-        image:"./images/O1.jpg",
+        titile:"Cachorros",
+        image:"./images/D2.jpg",
         name: "Harry"
     },
     {
         id:6,
         titile:"outros",
-        image:"./images/O2.jpg",
+        image:"./images/O1.jpg",
         name: "Loius"
     },
     {
         id:7,
         titile:"outros",
-        image:"./images/O3.jpg",
+        image:"./images/O2.jpg",
         name: "Valdineia"
     },
     {
         id:8,
         titile:"outros",
-        image:"./images/O4.jpg",
+        image:"./images/O3.jpg",
         name: "Ygor"
     },
 ]
